df24ab5bf941c0aabb384ebd3d8f4419836485f7
liptao <limpa0099@gmail.com>
2025-06-19 00:59:19 -0700

    Update lint errors

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const express = require('express')
+import express from 'express'
 const app = express()
 
 // get the port from env variable
@@ -7,5 +7,6 @@ const PORT = process.env.PORT || 5001
 app.use(express.static('dist'))
 
 app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
   console.log(`server started on port ${PORT}`)
 })
