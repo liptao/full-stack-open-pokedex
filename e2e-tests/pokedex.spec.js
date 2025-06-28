f6b5d79cd58c9a73e2863fb3ebcbb1900adc5a4e
liptao <limpa0099@gmail.com>
2025-06-27 18:01:19 -0700

    Add ivysaur page test

--- e2e-tests/pokedex.spec.js
+++ e2e-tests/pokedex.spec.js
@@ -5,5 +5,10 @@ describe('Pokedex', () => {
     await page.goto('http://localhost:8080/')
     await expect(page.getByText('ivysaur')).toBeVisible()
     await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
+  }),
+  test('ivysaur page displays Clorophyl', async ({ page }) => {
+    await page.goto('http://localhost:8080/')
+    await page.getByText('ivysaur').click()
+    await expect(page.getByText('Chlorophyll')).toBeVisible()
   })
 })
