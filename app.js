f81b7144272deb5719fe2ec5cd4a148b5f93e5b1
liptao <limpa0099@gmail.com>
2025-06-29 22:55:44 -0700

    Add health checks to deployment

--- app.js
+++ app.js
@@ -5,8 +5,15 @@ const app = express()
 const PORT = process.env.PORT || 5001
 
 app.use(express.static('dist'))
+
 app.get('/version', (req, res) => {
   res.send('1') // change this string to ensure a new version deployed
+})
+
+app.get('/health', (req, res) => {
+  // eslint-disable-next-line no-constant-condition
+  // if (true) throw('error...  ')
+  res.send('ok')
 })
 
 app.listen(PORT, () => {
